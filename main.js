ebdf8c7049e769bc78ceb40ff18c145beca66030
Roman <grabberisonfire@gmail.com>
2024-04-08 13:00:50 +0300

    version 0.2.0

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-const conn_positions = {
-    'right': 0,
-    'down-right': 1,
-    'down': 2,
-    'down-left': 3,
-    'left': 4,
-    'top-left': 5,
-    'top': 6,
-    'top-right': 7
-};
-
 function distance_to(other_object) {
     return Math.sqrt((this.x - other_object.x)**2 + (this.y - other_object.y)**2);
 }
@@ -52,9 +41,10 @@ class SelectDot {
 
 class Node {
    constructor(text) {
+       this.value = text;
        this.distance_to = distance_to;
 
-       this.radius = 50;
+       this.radius = 40;
        this.x = 50;
        this.y = 50;
 
@@ -101,6 +91,40 @@ class Node {
         for (let dot of this.dotObjects) {
             dot.object.style.display = "none";
             dot.off_state();
+        }
+   }
+   get_dot(dot_position) {
+       const conn_positions = {
+           'right': 0,
+           'down-right': 1,
+           'down': 2,
+           'down-left': 3,
+           'left': 4,
+           'top-left': 5,
+           'top': 6,
+           'top-right': 7
+       };
+      return this.dotObjects[conn_positions[dot_position]];
+   }
+   set_color(color) {
+      this.object.style.borderColor = color;
+      this.object.style.color = color;
+      this.object.style.borderWidth = '4px';
+   }
+   set_color_by_result(outcome, turn) {
+        switch(outcome + " " + turn) {
+            case "loss 0":
+                this.set_color("green");
+                break;
+            case "win 1":
+                this.set_color("orange");
+                break;
+            case "loss 1":
+                this.set_color("blue");
+                break;
+            case "win 2":
+                this.set_color("brown");
+                break;
         }
    }
 }
@@ -170,8 +194,8 @@ let connections = []
 
 function onMouseDown(event) {
     for (let node of nodes) {
-        x_diff = event.clientX - node.x;
-        y_diff = event.clientY - node.y;
+        x_diff = event.pageX - node.x;
+        y_diff = event.pageY - node.y;
         if (x_diff**2 + y_diff**2 <= node.radius**2) {
             cur_picked_node = nodes.indexOf(node);
             break;
@@ -180,8 +204,8 @@ function onMouseDown(event) {
         if (0 <= from_radius && from_radius <= 25) {
             node.on_select_state();
             for (let dot of node.dotObjects) {
-                let dot_x_diff = event.clientX - dot.x;
-                let dot_y_diff = event.clientY - dot.y;
+                let dot_x_diff = event.pageX - dot.x;
+                let dot_y_diff = event.pageY - dot.y;
                 if (dot_x_diff**2 + dot_y_diff**2 <= 40) {
                     dot.on_state();
                     cur_picked_dot = dot;
@@ -197,10 +221,10 @@ function onMouseDown(event) {
 function onMouseMove(event) {
    if (cur_picked_node !== -1) {
        let node = nodes[cur_picked_node];
-       node.move_to(event.clientX - node.radius - x_diff, event.clientY - node.radius - y_diff);
+       node.move_to(event.pageX - node.radius - x_diff, event.pageY - node.radius - y_diff);
    }
    if (cursor_dot !== null) {
-       cursor_dot.move_to(event.clientX, event.clientY);
+       cursor_dot.move_to(event.pageX, event.pageY);
        cursor_connection.update_coords();
    }
 }
@@ -211,14 +235,14 @@ function onMouseUp(event) {
     y_diff = 0;
     for (let node of nodes) {
         node.off_select_state();
-        x_diff = event.clientX - node.x;
-        y_diff = event.clientY - node.y;
+        x_diff = event.pageX - node.x;
+        y_diff = event.pageY - node.y;
 
         let from_radius = Math.sqrt(Math.abs(x_diff**2 + y_diff**2 - node.radius**2));
         if (0 <= from_radius && from_radius <= 25) {
             for (let dot of node.dotObjects) {
-                let dot_x_diff = event.clientX - dot.x;
-                let dot_y_diff = event.clientY - dot.y;
+                let dot_x_diff = event.pageX - dot.x;
+                let dot_y_diff = event.pageY - dot.y;
                 if (dot_x_diff**2 + dot_y_diff**2 <= 40) {
                     if (cur_picked_dot && cur_picked_dot.origin_node !== dot.origin_node) {
                         let new_connection = new Connection(cur_picked_dot, dot);
@@ -237,20 +261,65 @@ function onMouseUp(event) {
     cursor_connection = null;
 }
 
-if (typeof module !== 'undefined') {
-    module.exports = Dot;
-
-} else {
-    function handleNewNode(event) {
-       let new_node = new Node(add_new_element_text.value);
-       nodes.push(new_node);
-    }
-
-    add_new_element_text = document.getElementById("value-add-new-equation");
-    add_new_element_button = document.getElementById("add-new-equation");
-    add_new_element_button.addEventListener("click", handleNewNode);
+function handleNewNode(event) {
+   let new_node = new Node(add_new_element_text.value);
+   nodes.push(new_node);
 }
+
+add_new_element_text = document.getElementById("value-add-new-equation");
+add_new_element_button = document.getElementById("add-new-equation");
+add_new_element_button.addEventListener("click", handleNewNode);
 
 document.addEventListener('mousedown', onMouseDown);
 document.addEventListener('mousemove', onMouseMove);
 document.addEventListener('mouseup', onMouseUp);
+
+let operations = [x => x + 1, x => x * 2];
+
+const start_num = 5;
+const goal_num = 22;
+
+const recursion_depth = 6;
+const results = Array(4).fill([]);
+
+function solver(prev_node, turns_left) {
+    if (turns_left === 0) return ["win", 10**10];
+    const prev_value = parseInt(prev_node.value);
+    let found_loss = 10**10;
+    let found_win = 10**10;
+    for (let [op_num, operation] of operations.entries()) {
+        const new_value = operation(prev_value);
+        let new_node = new Node(new_value.toString());
+        nodes.push(new_node);
+        let new_conn = new Connection(prev_node.get_dot('right'),
+                                                  new_node.get_dot('left'));
+        connections.push(new_conn);
+        const new_x = prev_node.x + 200;
+        const new_y = prev_node.y + 2**(turns_left - 1) * 100 * op_num - new_node.radius;
+        new_node.move_to(new_x, new_y);
+        if (new_value < goal_num) {
+            let [result, val] = solver(new_node, turns_left - 1);
+            if (result === "loss") {
+                found_loss = Math.min(found_loss, val);
+            } else {
+                found_win = Math.min(found_win, val);
+            }
+        }
+        else {
+            new_node.set_color_by_result("loss", 0);
+            found_loss = 0;
+        }
+    }
+    let result;
+    if (found_loss !== 10**10)
+        result = ["win", found_loss + 1];
+    else
+        result = ["loss", found_win];
+    prev_node.set_color_by_result(result[0], result[1]);
+    return result;
+}
+
+const original_node = new Node(start_num.toString());
+original_node.move_to(50, 50);
+nodes.push(original_node);
+solver(original_node, recursion_depth);
--- main.test.js
+++
@@ -1,8 +0,0 @@
-const Dot = require("./main.js");
-
-test('should calculate distance properly', () => {
-  const pointA = new Dot(0, 0);
-  const pointB = new Dot(3, 4)
-  const result = pointA.distance_to(pointB);
-  expect(result).toBe(5);
-});
